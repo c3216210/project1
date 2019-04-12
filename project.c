4d7e7d7321445aa65c6b492da4debecb01cd103c
Daniel Tsou <c3216210@uon.edu.au>
2019-04-12 04:18:34 +0000

    assignment 1

--- project.c
+++ project.c
@@ -19,13 +19,26 @@ int main(){
             break ;
             }
             }
+    char key[25];
+   
+    input = fopen("key.txt", "r");
+    if(input == NULL) {
+        perror("fopen()");
+        }
+        for(i = 0; i<26; i++) {
 
-  
-  printf("The encrytion of rotation message:\n");
+        key[i] = fgetc(input);
+        if( feof(input) ) {
+            break ;
+            }
+            }
+   int n/*Set the initial*/, k = 1/* the key value*/, b = 0/*Set the initial*/;
+   
     /*Encrypt the rotation cipher message*/
-   int n/*Set the initial*/, k = 6/* the key value*/, b = 0/*Set the initial*/;
-     
-   for(n = 0; n<26; n++){
+
+   printf("The encrytion of rotation message:\n");
+   
+   for(n = 0; n< 26; n++){
        
        /*Read the given text then encrypt by the key*/
        b = a[n]+ k;
@@ -40,11 +53,13 @@ int main(){
        }
    }
    printf("\n");
-    printf("\n");
+   printf("\n");
     
    /*Decrypt the rotation cipher message*/
     printf("The dnecrytion of rotation message:\n");
-  int j = 0;/*the value of key*/
+  /*the value of key*/
+   int j = 0;
+   
    for(n = 0; n < 26; n++){
        
        /* Read the given text then decrypt by the key*/
@@ -60,18 +75,39 @@ int main(){
        printf("%c", b);
        }
    }
-       printf("\n");
-  printf("\n");
+    
+    
+    printf("\n");
+    printf("\n");
   
    /*Encrypt the substutution cipher message*/
     printf("The encrytion of substitution message:\n");
-
+    for(n = 0; n < 26; n++){
+        int r;
+        for(r = n; r < 26; r ++){
+           if(key[r]>96&&key[r]<123){
+           a[r] = key[r];
+           }
+           }
+    }  
+    printf("%s",a);
     printf("\n");
-      printf("\n");
-      
+    printf("\n");
    /*Decrypt the substitution cipher message*/
     printf("The decrytion of substitution message:\n");
+       for(n = 0; n < 26; n++){
+        int r;
+        for(r = n; r < 26; r ++){
+           if(key[r]>96&&key[r]<123){
+           a[r] = key[r];
+           }
+           }
+    }  
+    printf("%s",a);
 
+    printf("\n");
+    printf("\n");
+      
   
   
 
